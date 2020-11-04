96e19b101b885b46a2ac818ba061d870098c56c4
psosnowka <pawel.sosnowka@upaid.pl>
2020-11-04 22:10:41 +0100

    next

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,17 +1,33 @@
-import React, {Component, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './VideoCard.css'
+import VideoHeader from "./VideoHeader";
 
 function VideoCard() {
     const [isVideoPlaying, setIsVideoPlaying] = useState(false)
-    
+    const videoRef = useRef(null)
+    const onVideoPress = () => {
+        if (isVideoPlaying) {
+            // stop video
+            videoRef.current.pause();
+            setIsVideoPlaying(false)
+        } else {
+            videoRef.current.play()
+            setIsVideoPlaying(true)
+            //start video
+        }
+    }
     return (
         <div className="videoCard">
-            <img
+            <VideoHeader></VideoHeader>
+            <video
+                ref={videoRef}
+                onClick={onVideoPress}
                 className='video__player'
-                src='./instagram_logo.png'
                 alt='IG reel video'
-                // loop
-            />
+                loop>
+                <source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm"/>
+                {/*<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg"/>*/}
+            </video>
         </div>
     );
 
